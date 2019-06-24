98fac96135036b3ec80a266153ab097e12381405
Ghasak Ibrahim <mdotgh@gmail.com>
2019-06-24 17:42:14 +0900

    Added P2

--- coding_problems/src/P2_Compare_current_item_to_next_item.py
+++ coding_problems/src/P2_Compare_current_item_to_next_item.py
@@ -1,6 +1,9 @@
 # More details overthis issue can be found here
 #https://stackoverflow.com/questions/24577688/compare-current-item-to-next-item-in-a-python-list
 
+
+
+
 mylist = [1, 2, 2, 4, 2, 3]
 for i, j in enumerate(mylist[:-1]):
     if j  == mylist[i+1]:
@@ -12,5 +15,9 @@ print (mylist)
 j = 1
 compare = [1,2,5,3,2,4,4,2,6]
 for item in compare:
+    if j == len(compare):
+        break
     print(item,compare[j])
     j +=1
+
+
